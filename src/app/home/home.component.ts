bf24144931734557bc91faf45f911f00c2410e1a
danielchungara1 <danielchungara1@gmail.com>
2020-03-19 19:32:40 -0300

    fix naveganilidad

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -12,7 +12,7 @@ export class HomeComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    this.router.navigate(['/busqueda-producto'], { queryParams: { search: '' }});
+    this.router.navigate(['/busqueda-producto']);
   }
 
 }
